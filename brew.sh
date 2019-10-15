760f1b6b8350ad0de03eab3cb53b298f25077ab1
Tim Barlotta <tim@barlotta.net>
2019-10-14 21:28:30 -0400

    Changes for homebrew applications

--- brew.sh
+++ brew.sh
@@ -21,7 +21,7 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed
 # Install a modern version of Bash.
 brew install bash
 brew install bash-completion2
@@ -32,70 +32,89 @@ if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
+# Install `wget`
+brew install wget
 
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
+#brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
 brew install screen
-brew install php
-brew install gmp
+#brew install php
+#brew install gmp
 
 # Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+#brew tap bramstein/webfonttools
+#brew install sfnt2woff
+#brew install sfnt2woff-zopfli
+#brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
+#brew install aircrack-ng
+#brew install bfg
+#brew install binutils
+#brew install binwalk
+#brew install cifer
+#brew install dex2jar
+#brew install dns2tcp
+#brew install fcrackzip
+#brew install foremost
+#brew install hashpump
+#brew install hydra
+#brew install john
+#brew install knock
+#brew install netpbm
 brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+#brew install pngcheck
+#brew install socat
+#brew install sqlmap
+#brew install tcpflow
+#brew install tcpreplay
+#brew install tcptrace
+#brew install ucspi-tcp # `tcpserver` etc.
+#brew install xpdf
+#brew install xz
 
 # Install other useful binaries.
-brew install ack
+#brew install ack
 #brew install exiv2
 brew install git
 brew install git-lfs
-brew install gs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
+#brew install gs
+#brew install imagemagick --with-webp
+#brew install lua
+#brew install lynx
+#brew install p7zip
+#brew install pigz
+#brew install pv
+#brew install rename
+#brew install rlwrap
+#brew install ssh-copy-id
+#brew install tree
+#brew install vbindiff
+#brew install zopfli
+brew install micro
+brew install icdiff
+brew install mosh
+brew install restic
+brew install subversion
+
+# install java 
+brew tap AdoptOpenJDK/openjdk
+brew cask install adoptopenjdk
+brew cask install adoptopenjdk11
+brew cask install adoptopenjdk8
+
+# install java utils
+brew install ant
+brew install gradle
+brew install maven
+
+# languages
+brew install go
 
 # Remove outdated versions from the cellar.
 brew cleanup
